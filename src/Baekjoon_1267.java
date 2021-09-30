f842309d397da70c27e24e79fe66d524e5ad01bb
yys7517 <yys7517@naver.com>
2021-09-30 21:57:59 +0900

    JAVA Algorithm Study 21_09_30

---
+++ src/Baekjoon_1267.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1267 {
+    // 핸드폰 요금
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+        int N = Integer.parseInt( br.readLine() );
+        int [] sec = new int[ N ];
+        int Yfee = 0;       // 영식 요금제로 했을 때 총 요금.
+        int Mfee = 0;       // 민식 요금제로 했을 때 총 요금.
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for ( int i = 0 ; i < N; i++ ) {
+            sec[i] = Integer.parseInt( st.nextToken() );
+        }
+        // System.out.println(Arrays.toString(sec) );
+
+        // 영식 요금제 : 0 ~ 29초 10원, 30초 왜 20원 ?
+        // 민식 요금제 : 0 ~ 59초 15원, 60초 왜 30원 ?
+        for ( int i = 0; i < N; i++ ) {
+            Yfee += ( ( sec[i] / 30 ) + 1)  * 10;
+            Mfee += ( ( sec[i] / 60 ) + 1)  * 15;
+        }
+
+        //System.out.println(Yfee);
+        //System.out.println(Mfee);
+
+        // 민식요금제로 계산한 총 요금이 비쌀 때...
+        if ( Yfee < Mfee )
+            System.out.println("Y "+ Yfee);
+        else if ( Yfee > Mfee )
+            System.out.println("M "+ Mfee);
+        else
+            System.out.println("Y M "+Yfee);
+    }
+}
---
+++ src/Baekjoon_1333.java
@@ -0,0 +1,60 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class Baekjoon_1333 {
+    // 부재중 전화
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt( st.nextToken() );       // 노래 개수 N                 2
+        int L = Integer.parseInt( st.nextToken() );       // 노래 길이 L                 5
+        int D = Integer.parseInt( st.nextToken() );       // 전화벨이 D초에 1번씩 울림.  7
+
+        // 노래가 나오는 동안은 true
+        // 노래가 안나오는 동안은 false로 해보자.
+
+        int term = 5; // 노래 사이 간격.
+
+        Vector<Boolean> v = new Vector<Boolean>( 20);
+
+        for ( int i = 0 ; i < N; i++ ) {
+            // 노래 길이 만큼     true ( 노래 나온다. )
+            for ( int j = 0; j < L; j++ ) {
+                v.add(true);
+            }
+
+            // 마지막 곡이 아니라면.
+            if ( i != N-1 ) {
+                // 노래 사이 간격만큼 false ( 노래 안나온다. )
+                for ( int k = 0; k < term; k++ ) {
+                    v.add(false);
+                }
+            }
+        }
+        /* Vector 출력해서 확인해보자.
+        Iterator<Boolean> ir = v.iterator();
+
+        while ( ir.hasNext() ) {
+            Boolean b = ir.next();
+            System.out.printf("[%b] ", b );
+        } */
+
+        int result = 0;
+
+        // Vector의 인덱스를 시간으로 할 수 있음.
+        int size = v.size();
+        while ( result < size ) {
+            if (v.get(result) == false) {
+                break;
+            } else
+                result += D;        // 전화벨 간격 D만큼 증가..
+        }
+        System.out.println( result );
+    }
+}
