808dcebb4a2f923834e0869b26e9437425e5a7f3
yys7517 <yys7517@naver.com>
2021-10-11 23:51:37 +0900

    JAVA Algorithm Study 21_10_11

---
+++ src/Baekjoon_1212.java
@@ -0,0 +1,104 @@
+
+import java.util.Scanner;
+
+public class Baekjoon_1212 {
+    // 8진수 -> 2진수
+    static String[] octToBin = {"000", "001", "010", "011", "100", "101", "110", "111"};
+
+    public static void main(String[] args)  {
+        Scanner sc  = new Scanner( System.in );
+        char [] oct = sc.next().toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        if ( oct.length == 1 && oct[0] == '0') {
+            System.out.println( 0 );
+            return;
+        }
+
+        for ( int i = 0; i < oct.length; i++ ) {
+            sb.append( octToBin[oct[i] -'0'] );
+        }
+
+        while ( sb.charAt(0) == '0' ) {
+            sb.deleteCharAt(0);
+        }
+
+        // 8진수 -> 2진수 변환. 8진수 1자리 값을 -> 3자리 2진수 값으로 변환 가능.
+        /*
+        for ( int i = 0; i < sb.length(); i++ ){
+            String value = "";
+            int a = Integer.parseInt (String.valueOf(sb.charAt(i)) );
+            System.out.println( a );
+            if ( a >= 0 && a <= 7 ) {
+                if  ( a == 0 ) {
+                    System.out.println( 0 );
+                    break;
+                }
+                else {
+                    while( a > 0 ) {
+                        int rest = a % 2;
+                        value = rest + value;
+                        a /= 2;
+                    }
+                    if ( i == 0 ) {
+                        sb1.append( String.format("%d", Integer.parseInt(value)) );
+                    }
+                    else
+                        sb1.append( String.format("%03d", Integer.parseInt(value)) );
+                }
+            }
+        }*/
+
+        System.out.println( sb );
+
+
+
+        // 8진수 -> 10진수 -> 2진수 변환 (비효율적..)
+        /*
+        StringBuilder sb = new StringBuilder( input );
+        int [] inputnum = new int[ input.length() ];
+        int decimal = 0;
+
+        for ( int i = 0; i < input.length(); i++ ){
+           inputnum[i] = Integer.parseInt (String.valueOf(sb.charAt(i)) );
+           decimal += inputnum[i] * square(8, inputnum.length - 1 -i );
+        }
+        // System.out.println(Arrays.toString( inputnum ));    // 입력 값을 배열에 받아옴.
+        // System.out.println( decimal );                      // 10 진수로 변환한 값 decimal
+
+        int cnt = 0;
+        int temp = decimal;
+        while ( temp > 0 ) {
+            temp /= 2;
+            cnt++;
+        }
+        //System.out.println( cnt );
+        int [] result = new int[cnt];
+
+        int share;
+        while ( decimal > 0 ) {
+            share = decimal / 2;
+            int rest = decimal % 2;
+            result[cnt-1] = rest;
+            decimal /= 2;
+            cnt--;
+        }
+        for ( int i = 0; i < result.length; i++ ) {
+            System.out.print(result[i]);
+        }
+         //System.out.println( Arrays.toString( result ) );
+    }
+
+    private static int square(int a, int b) {
+        if ( b == 0 )
+            return 1;
+        else {
+            for ( int i = 1; i < b; i++ ){
+                a *= a;
+            }
+            return a;
+        }
+*/
+
+    }
+}
---
+++ src/Baekjoon_2775.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Baekjoon_2775 {
+    // 부녀회장이 될테야
+    // a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼 사람들을 데려와 살아야 한다”
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ));
+        int Testcase = Integer.parseInt( br.readLine() );
+        ArrayList<Integer> resultlist= new ArrayList<>();
+
+        for ( int i = 0; i < Testcase; i++ ) {
+            // k층의 n호에는 몇 사람이 살고 있을까 ?
+            int k = Integer.parseInt( br.readLine() );
+            int n = Integer.parseInt( br.readLine() );
+            int [][] result = new int[k+1][n];
+
+            for ( int j = 0; j < n; j++ ) {
+                result[0][j] = j+1;
+            }
+            //System.out.println( Arrays.toString( result[0] ));
+
+            for ( int a = 1; a < result.length; a++ ) {
+                for ( int b = 0; b < result[0].length; b++ ) {
+                    int cnt = 0;
+                    while ( cnt <= b ) {
+                        result[a][b] += result[a-1][cnt];
+                        cnt++;
+                    }
+                }
+                //System.out.println(Arrays.toString( result[a] ) );
+            }
+            resultlist.add( result[k][n-1] );
+
+
+        }
+        for ( int resulti : resultlist )
+            System.out.println( resulti );
+
+        br.close();
+
+    }
+}
