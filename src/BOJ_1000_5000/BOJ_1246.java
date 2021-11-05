86324f39b3870870c56cd017ca1bf6ab34fae8d6
yys7517 <yys7517@naver.com>
2021-11-06 01:50:59 +0900

    JAVA Algorithm Study 21_11_05

--- src/BOJ_1000_5000/BOJ_1246.java
+++ src/BOJ_1000_5000/BOJ_1246.java
@@ -33,7 +33,11 @@ public class BOJ_1246 {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
+        // M명의 고객이 제시한 가격 배열.
         int[] price = new int[M];
+
+        // 고객이 제시한 가격의 최대값을 구하는 이유 ?
+        // 1원부터 하여 그 가격까지 옥션에서 판매를 할 때, 그 때의 총 수익을 확인하기 위해서.
         int maxPrice = 0;
         for (int i = 0; i < M; i++) {
             price[i] = sc.nextInt();
@@ -50,28 +54,44 @@ public class BOJ_1246 {
         // System.out.println( maxPrice );  10
 
         Arrays.sort(price);
+        // 정렬을 굳이 하지 않아도 되더라..
 
-        int[] result = new int[maxPrice + 1];
+
+        // 총 수익을 담는 배열 totalPrice
+        // totalPrice의 i번째 값은 i 원에 판매를 할 때의 총 수익 값이 들어가게 된다.
+        // 1원부터 maxPrice의 가격에 판매하게 될 때 수익들이 배열에 저장된다.
+        // 0번 인덱스는 사용하지말자.
+        int[] totalPrice = new int[maxPrice + 1];
         int MinPrice = 1;
+
+        // 최대 수익을 구하자.
         int maxTotalPrice = 0;
+        // 1원에 판매됬을 때 총 수익부터
+        // maxPrice에 판매할 때 총 수익을 구하자.
         for (int i = MinPrice; i <= maxPrice; i++) {
-            int temp = N;
+            int temp = N;               // 계란의 개수만큼 판매할 수 있기 때문에
             for (int j = 0; j < price.length; j++) {
-                if (i <= price[j] && temp > 0) {
-                    result[i] += i;
-                    temp--;
+                if (i <= price[j] && temp > 0) {        // 판매하는 가격 i가 고객이 제시한 가격보다 작거나 같을 때, 고객은 구입한다.
+                    totalPrice[i] += i; // 수익 i 만큼 증가. ( 판매가격 : i )
+                    temp--;             // 달걀 개수 -1
                 }
             }
-            if (maxTotalPrice < result[i])
-                maxTotalPrice = result[i];
+            if (maxTotalPrice < totalPrice[i])
+                maxTotalPrice = totalPrice[i];
         }
         // System.out.println( maxTotalPrice );
         // [0, 4, 8, 9, 12, 15, 18, 21, 16, 9, 10]
-        // System.out.println( Arrays.toString( result ));
-        for (int i = 1; i < result.length; i++) {
-            if (result[i] == maxTotalPrice) {
+        // System.out.println( Arrays.toString( totalPrice ));
+
+        // 0번 인덱스를 사용하지 않았으므로 1번 인덱스부터 확인.
+        // 1원에 판매됬을 때 총 수익 부터 2원..3원.. 확인한다는 뜻.
+        // 최대 수익과 같을 때의, 그 때 가격 i와, 최대 수익 값을 출력.
+        // 최대 수익과 같을 때의 달걀 한 개당 최소의 가격을 구하는 것이기 때문에, 1부터 for문
+
+        for (int i = 1; i < totalPrice.length; i++) {
+            if (totalPrice[i] == maxTotalPrice) {
                 System.out.print(i + " " + maxTotalPrice);
-                break;
+                break;                  // 달걀 한 개의 최소의 가격을 구하는 것이기 때문에, 출력하고 break 반복을 중지.
             }
         }
 
--- src/BOJ_20000_25000/BOJ_22993.java
+++ src/BOJ_20000_25000/BOJ_22993.java
@@ -16,28 +16,41 @@ public class BOJ_22993 {
         }
         // 준원이의 공격력은 power[0]
         long jw = power[0];
+
+        // 전체공격력의 합을 구하자.
         long powersum = 0;
         for (int i = 0; i < power.length; i++) {
             powersum += power[i];
         }
+        // 전체 공격력의 합을 구하는 이유는 최종 생존자는 전체 공격력의 합에 해당하는 공격력을 가지게 된다.
 
+        // 준원이를 제외한 사람들의 공격력을 배열로 만든다.
+        // power 1번 인덱스부터 N-1번 인덱스까지의 배열을 복사한다.
         int [] others = Arrays.copyOfRange( power , 1 , N);
         // System.out.println( powersum );
         // System.out.println( Arrays.toString( others ));
 
+        // 준원이는 어떤 순서로든 이기기만 하면되므로, 제일 약한 상대와 싸워가면서 최종 생존하게 될 것이다.
+        // others 배열을 정렬시키면, 가장 낮은 공격력부터 높은 공격력 순서대로 정렬이 되므로 낮은 공격력의 상대와 비교해간다.
         Arrays.sort( others );
         for (int i = 0; i < others.length; i++) {
+            // 준원이가 공격력이 더 높고, 그 사람이 죽지 않았으면.
             if ( jw > others[i] && others[i] != 0 ) {
-                jw += others[i];
-                others[i] = 0;
+                jw += others[i];        // 준원이는 승리하고, 그 사람의 공격력만큼을 가져간다.
+                others[i] = 0;          // 죽은사람은 공격력을 0으로 바꿔준다.
             }
             else
-                break;
+                break;              // 오름차순 정렬을 하였으니, 준원이보다 공격력이 높은 상대를 만난다면 반복문을 진행할 필요 X
         }
-        System.out.println( Arrays.toString( others ));
+
+        // 준원이를 제외한 다른 사람들의 배열을 출력하여 죽었는 지( 값 : 0 ? ) 확인하였다.
+        // System.out.println( Arrays.toString( others ));
+
+        // 준원이의 공격력이 전체 공격력 합과 같다면 최종 생존
         if ( jw == powersum ) {
             System.out.print("Yes");
         }
+        // 아니라면 준원이는 생존하지 못했다.
         else
             System.out.println("No");
 
