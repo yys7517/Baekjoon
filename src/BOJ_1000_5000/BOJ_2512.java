2e4193d5171d0cd5cb6525fbd15012ef36222cdd
yys7517 <yys7517@naver.com>
2022-01-03 21:13:09 +0900

    COMMIT. JAVA Algorithm Study 22_01_03 BOJ 2512 예산 수정.

--- src/BOJ_1000_5000/BOJ_2512.java
+++ src/BOJ_1000_5000/BOJ_2512.java
@@ -3,7 +3,6 @@ package BOJ_1000_5000;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -22,32 +21,34 @@ public class BOJ_2512 {
 
     private static BufferedReader br = new BufferedReader( new InputStreamReader( System.in ));
 
+    private static long budget;
+    private static long max = 0;
+
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt( br.readLine() );
         StringTokenizer token = new StringTokenizer( br.readLine() , " ");
         int [] arr = new int[ N ];
-        long max = 0;
+
         long sum = 0;
-        int idx = 0;
-        while ( token.hasMoreTokens() ){
+
+        for (int i = 0; i < N; i++) {
             int tmp = Integer.parseInt(token.nextToken() );
-            arr[idx] = tmp;
+            arr[i] = tmp;
             sum += tmp;
             max = Math.max( max, tmp );
-            idx++;
         }
 
-        long budget = Integer.parseInt( br.readLine() );
+        budget = Integer.parseInt( br.readLine() );
         // System.out.println(Arrays.toString(arr) );
         // 총 예산으로 모든 요청에 배정될 수 있다면
         // 배정된 예산의 최대값을 출력.
-        if ( sum <= budget ) {
-            System.out.println( max );
-        }
-        // 총 예산으로 모든 요청에 배정될 수 없다면
-        // 상한액을 정해야 함.
-        else {
 
+        long result;
+
+        if ( sum <= budget )
+            result = max;
+
+        else {
             // 상한액 = 121
             //
             //tmp = 0;
@@ -75,24 +76,22 @@ public class BOJ_2512 {
 //            result += ( tmp / count );
 //
 //            System.out.println( result );
-            int result = ParameterSearch( arr , budget );
-            System.out.println( result );
+            result = ParameterSearch( arr );
+            // 총 예산으로 모든 요청에 배정될 수 없다면
+            // 상한액을 정해야 함.
         }
-
+        System.out.println( result );
     }
 
-    private static int ParameterSearch( int[] arr , long budget) {
-        // 정렬
-        Arrays.sort( arr );
-
+    private static long ParameterSearch( int[] arr ) {
         // 최소값
-        int start = 0;
+        long start = 0;
 
         // 최대값 ( 마지노선 )
-        int end = arr[ arr.length - 1 ];
+        long end = max;
 
         while ( start <= end ) {        // 최소값이 마지노선과 일치해지거나 높아지면 반복을 중단한다.
-            int mid = ( start + end ) / 2;  //  현재 상한가
+            long mid = ( start + end ) / 2;  //  현재 상한가
 
             long current = 0;       // 총 사용 예산
 
@@ -102,13 +101,15 @@ public class BOJ_2512 {
                 if( arr[i] < mid ) {
                     current += arr[i];
                 }
+
+
                 // 상한가보다 요청 예산이 크다면 ?
                 // 상한가만 더해준다.
-
+                // Why? 상한가를 더해주는 이유는.. 모든 예산 요청이 배정될 수 없는 경우이므로
+                // 그 이상인 예산 요청에는 모두 상한액을 배정
                 else
                     current += mid;
-                // Why? 상한가를 더해주는 이유는.. 전체 요청 예산이 우리가 가지고 있는 budget보다 많기 때문에
-                // 상한가를 더해줘야 나눠주지 못하는 지방 단체에게 공평하게 나눠줄 수 있다.
+
             }
 
             if( current < budget ) {
@@ -116,17 +117,25 @@ public class BOJ_2512 {
                 // 가지고 있는 예산 budget이 더 클 때, 상한가를 더 높여도 된다.
                 // 어차피 이 과정을 반복하는 while문은 마지노선(최대값)을 만나거나 넘어설 때, 종료한다.
             }
-            else if ( current > budget ){   // 가지고 있는 예산 budget보다 상한가를 통해 나눠주는 예산이 많을 때
+            else if ( current > budget ){   // 가지고 있는 예산 budget보다 사용될 예산 current 가 많을 때 ?
                 end = mid - 1;              // budget보다 많은 예산 current는 나눠줄 수 없다.
                 // 상한가 mid 를 내리기 위해, 마지노선인 end 값을 낮춘다.
             }
             // 상한가를 통해 나눠준 예산의 합이 우리가 가지고 있는 총 예산 budget과 일치할 때
+            // 일치할 때, 이 때가 최대 상한액이 될 것이다.
+
+            // 이 경우가 걸리지 않더라도, start가 end 값을 넘어서는 시점에서 while문은 종료된다.
             else
                 return mid;
-        }
+        }   // while
 
-        // 최대 예산을 구하는 것이므로.
-        // 반복문이 종료되었을 때, end값 (마지노선 == 최댓값)을 반환해준다.
+        // 반복문이 종료되는 조건 == start > end
+        // 마지노선 값이 end를 return
         return end;
+
+        // start가 upper bound가 되는 것.
+        // upper bound - 1인 start - 1의 값을 return 해줘도 된다.
+        // return start - 1;
+
     }
 }
