b31ce53c942385fb112b18ff3af86f106125db52
yys7517 <yys7517@naver.com>
2022-01-04 18:09:13 +0900

    COMMIT. JAVA Algorithm Study 22_01_04 BOJ_10826. 피보나치 수 4

--- src/BOJ_10000_15000/BOJ_10826.java
+++ src/BOJ_10000_15000/BOJ_10826.java
@@ -8,12 +8,16 @@ import java.math.BigInteger;
 public class BOJ_10826 {
     // 피보나치 수 4
 
+    // 피보나치 수 다른 번호 문제들과 문제는 동일하다.
+    // 하지만 주어진 n의 범위가 다르다.
+
     // n은 10,000보다 작거나 같은 자연수 또는 0이다.
     // n의 값은 0 또는 1 ~ 10000
 
     //  다이나믹 프로그래밍
     //  임의 정밀도 / 큰 수( Java : BigInteger )  연산
 
+    // BigInteger 자료형 배열 arr
     private static BigInteger [] arr;
 
     public static void main(String[] args) throws IOException {
@@ -25,9 +29,10 @@ public class BOJ_10826 {
         else {
             arr = new BigInteger[ n+1 ];
 
-            arr[ 0 ] = BigInteger.ZERO;
-            arr[ 1 ] = BigInteger.ONE;
+            arr[ 0 ] = BigInteger.ZERO; // 0
+            arr[ 1 ] = BigInteger.ONE;  // 1
 
+            // BigInteger.add( BigInteger b )
             for (int i = 2; i <= n; i++) {
                 arr[ i ] = arr[i-2].add( arr[i-1] );
             }
