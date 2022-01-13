0316858ba4c2e214728796b41a6eed8e34b0bc9e
yys7517 <yys7517@naver.com>
2022-01-13 21:27:30 +0900

    COMMIT. JAVA Algorithm Study 22_01_13 BOJ_9184. 신나는 함수 실행 주석 수정.

--- src/BOJ_5000_10000/BOJ_9184.java
+++ src/BOJ_5000_10000/BOJ_9184.java
@@ -41,10 +41,10 @@ public class BOJ_9184 {
         StringTokenizer st;
 
         // 수가 0이하인 수가 하나라도 있을 경우
-        // 함수 w는 1을 return하므로
-        // 우리는 1 ~ 50의 인덱스만 필요로 한다.
-        // 0 ~ 50
-        arr = new int[ 51 ][ 51 ][ 51 ];
+        // 함수 w는 1을 return 하고
+        // 20을 초과하는 수가 하나라도 있을 경우 w(20,20,20)을 호출하므로
+        // a,b,c가 0 ~ 20의 값을 만족할 때 의 값만 저장하여 사용한다.
+        arr = new int[ 21 ][ 21 ][ 21 ];
 
         while ( true ) {
             st = new StringTokenizer( br.readLine() , " ");
@@ -65,17 +65,24 @@ public class BOJ_9184 {
 
 
 
-    // 메모이제이션 기법
+    // 메모이제이션 기법 ( 동적계획법 )
+
+    // 처음방문하는 재귀라면 계산을 통해 얻은 값을 메모리에 저장하고,
+    // 이 후 재방문을 할 경우, 계산을 다시 하지 않고 메모리에 저장된 값을 사용하는 것.
 
     private static int w(int a, int b, int c) {
 
         if( a <= 0 || b <= 0 || c <= 0 )
             return 1;
 
+        // 하나라도 20을 초과할경우, w(20,20,20)을 호출하게 되므로,
+        // 배열 w(20,20,20)의 값 까지만 우리는 저장하면 된다.
+        // 따라서 배열 arr의 마지막 값은 arr[20][20][20]이 되므로
         else if( a > 20 || b > 20 || c > 20 )
             return arr[20][20][20] = w(20,20,20);
 
-        // 초기값 0이 아닐 경우, 값을 출력.
+        // 초기화된 arr의 기본 값인 0이 아닌 어떤 값이 있고,
+        // a,b,c의 범위가 0 <= a,b,c <= 20 이라면 메모리에 저장된 값을 반환한다.
         else if( inRange(a,b,c) && arr[a][b][c] != 0 )
             return arr[a][b][c];
 
@@ -91,6 +98,7 @@ public class BOJ_9184 {
 
     }
 
+    // 0
     private static boolean inRange( int a, int b, int c ) {
         if( a >= 0 && a <= 20 && b >= 0 && b <= 20 && c >= 0 && c <= 20 )
             return true;
